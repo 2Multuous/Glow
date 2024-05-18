9776888c28fa538123f27d3860d90a255be134c7
2Multuous <132314186+2Multuous@users.noreply.github.com>
2024-05-18 13:46:34 -0400

    Add Firefly

---
+++ Firefly.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Firefly extends GameObject {
+	
+	public Firefly(double x, double y, double width, double height, double brightness, Color color) {
+		setX(x);
+		setY(y);
+		setWidth(width);
+		setHeight(height);
+		setBrightness(brightness);
+		setColor(color);
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(getColor());
+		g.fillRect((int)(getX()), (int)(getY()), (int)(getWidth()), (int)(getHeight()));
+	}
+}
