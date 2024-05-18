8143ed415289a7da3a154ecf8e54c9ef868daeb2
EJBanta <120134769+EJBanta@users.noreply.github.com>
2024-05-18 14:15:04 -0400

    Added Flare.java and Cube.java

---
+++ Cube.java
@@ -0,0 +1,66 @@
+import java.awt.*;
+
+public class Cube extends GameObject {
+    private final int WIDTH;
+    private final int HEIGHT;
+
+    private int numFlares;
+
+    public Cube(double x, double y, double direction, double width, int WIDTH, int HEIGHT) {
+        setX(x);
+        setY(y);
+        setDirection(direction);
+        setWidth(width);
+
+        this.WIDTH = WIDTH;
+        this.HEIGHT = HEIGHT;
+
+        numFlares = 0;
+    }
+
+    public Cube(double width, int WIDTH, int HEIGHT) {
+        this.WIDTH = WIDTH;
+        this.HEIGHT = HEIGHT;
+
+        setX(0);
+        setY(0);
+        setWidth(width);
+    }
+
+    public void drawCube(Graphics g, int mouseX, int mouseY, boolean mouseDown) {
+        move(mouseX, mouseY, mouseDown);
+
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setColor(getColor()); // Color: 5, 252, 248 ?
+
+        // (int)(getX() - getWidth()/2), (int)(getY() - getHeight()/2), (int)getWidth(), (int)getHeight()
+        Rectangle rect = new Rectangle(WIDTH/2, HEIGHT/2, (int)getWidth(), (int)getWidth());
+
+        g2d.rotate(Math.atan2(mouseY, mouseX));
+        g2d.draw(rect);
+        g2d.fill(rect);
+    }
+
+    public void move(int mouseX, int mouseY, boolean mouseDown) {
+        setDirection(Math.atan2(mouseY, mouseX));
+
+        if (mouseDown) {
+            setX(getX() + Math.cos(getDirection()));
+            setY(getY() + Math.sin(getDirection()));
+        }
+    }
+
+    public void grow(int widthAdded) {
+        setWidth(getWidth() + widthAdded);
+    }
+
+    public void dropFlare() {
+        numFlares--;
+    }
+
+    // Dummy method
+    @Override
+    public void draw(double cubeX, double cubeY, Graphics g) {
+        // THIS DOES NOTHING
+    }
+}
---
+++ Flare.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+
+public class Flare extends GameObject {
+    private boolean consumed;
+
+    public Flare(int x, int y) {
+        setX(x);
+        setY(y);
+        setWidth(4);
+        consumed = false;
+    }
+
+    @Override
+    public void draw(double cubeX, double cubeY, Graphics g) {
+        g.setColor(getColor());
+        g.drawRect((int)(getX() - cubeX - getWidth()/2), (int)(getY() - cubeY - getWidth()/2), (int)getWidth(), (int)getWidth());
+    }
+
+    public void consume() {
+        consumed = true;
+    }
+}
