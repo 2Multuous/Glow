da1defca6640d0ae4231c3045b59242457ece925
TheSummerCompany <judah@k2d2.org>
2024-05-18 21:54:35 -0400

    Update Beam.java

--- Beam.java
+++ Beam.java
@@ -20,8 +20,21 @@ public class Beam {
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setColor(color);
-        g2d.setStroke(new BasicStroke((int)width));
-        g2d.drawLine(WIDTH/2, HEIGHT/2, (int)(WIDTH/2 + 2000 * Math.cos(direction)), (int)(HEIGHT/2 + 2000 * Math.sin(direction)));
+        Color c = new Color(color.getRGB());
+        int red = color.getRed();
+        int green = color.getGreen();
+        int blue = color.getBlue();
+        g2d.setStroke(new BasicStroke((int) width));
+        g2d.drawLine(WIDTH/2, HEIGHT/2, (int) (WIDTH/2 + 2000 * Math.cos(direction)), (int) (HEIGHT/2 + 2000 * Math.sin(direction)));
+        g2d.setColor(new Color(red + (255 - red) / 8 * 2, green + (255 - green) / 8 * 2, blue + (255 - blue) / 8 * 2));
+        g2d.setStroke(new BasicStroke((int) width / 2));
+        g2d.drawLine(WIDTH/2, HEIGHT/2, (int) (WIDTH/2 + 2000 * Math.cos(direction)), (int) (HEIGHT/2 + 2000 * Math.sin(direction)));
+        g2d.setColor(new Color(red + (255 - red) / 8 * 4, green + (255 - green) / 8 * 4, blue + (255 - blue) / 8 * 4));
+        g2d.setStroke(new BasicStroke((int) width / 4));
+        g2d.drawLine(WIDTH/2, HEIGHT/2, (int) (WIDTH/2 + 2000 * Math.cos(direction)), (int) (HEIGHT/2 + 2000 * Math.sin(direction)));
+        g2d.setColor(new Color(red + (255 - red) / 8 * 8, green + (255 - green) / 8 * 8, blue + (255 - blue) / 8 * 8));
+        g2d.setStroke(new BasicStroke((int) width / 8));
+        g2d.drawLine(WIDTH/2, HEIGHT/2, (int) (WIDTH/2 + 2000 * Math.cos(direction)), (int) (HEIGHT/2 + 2000 * Math.sin(direction)));
         g2d.setStroke(new BasicStroke(1));
     }
 
