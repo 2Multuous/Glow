7d3bf9a32d2f982664aeff1703cb7f2e977c7d4c
EJBanta <120134769+EJBanta@users.noreply.github.com>
2024-05-18 23:22:24 -0400

    Update Beam.java

--- Beam.java
+++ Beam.java
@@ -1,19 +1,13 @@
 import java.awt.*;
 
 public class Beam {
-    private final int WIDTH;
-    private final int HEIGHT;
-
     private double direction;
     private double width;
     private Color color;
 
-    public Beam(double direction, Color color, int WIDTH, int HEIGHT) {
+    public Beam(double direction, Color color) {
         this.direction = direction;
         this.color = color;
-
-        this.WIDTH = WIDTH;
-        this.HEIGHT = HEIGHT;
     }
 
     public void draw(Graphics g) {
@@ -21,7 +15,7 @@ public class Beam {
 
         g2d.setColor(color);
         g2d.setStroke(new BasicStroke((int)width));
-        g2d.drawLine(WIDTH/2, HEIGHT/2, (int)(WIDTH/2 + 2000 * Math.cos(direction)), (int)(HEIGHT/2 + 2000 * Math.sin(direction)));
+        g2d.drawLine(GlowAnimation.WIDTH/2, GlowAnimation.HEIGHT/2, (int)(GlowAnimation.WIDTH/2 + 2000 * Math.cos(direction)), (int)(GlowAnimation.HEIGHT/2 + 2000 * Math.sin(direction)));
         g2d.setStroke(new BasicStroke(1));
 
         // Cool looking but non-revealing beam
@@ -33,16 +27,16 @@ public class Beam {
 //        int green = color.getGreen();
 //        int blue = color.getBlue();
 //        g2d.setStroke(new BasicStroke((int) width));
-//        g2d.drawLine(WIDTH/2, HEIGHT/2, (int) (WIDTH/2 + 2000 * Math.cos(direction)), (int) (HEIGHT/2 + 2000 * Math.sin(direction)));
+//        g2d.drawLine(GlowAnimation.WIDTH/2, GlowAnimation.HEIGHT/2, (int) (GlowAnimation.WIDTH/2 + 2000 * Math.cos(direction)), (int) (GlowAnimation.HEIGHT/2 + 2000 * Math.sin(direction)));
 //        g2d.setColor(new Color(red + (255 - red) / 8 * 2, green + (255 - green) / 8 * 2, blue + (255 - blue) / 8 * 2));
 //        g2d.setStroke(new BasicStroke((int) width / 2));
-//        g2d.drawLine(WIDTH/2, HEIGHT/2, (int) (WIDTH/2 + 2000 * Math.cos(direction)), (int) (HEIGHT/2 + 2000 * Math.sin(direction)));
+//        g2d.drawLine(GlowAnimation.WIDTH/2, GlowAnimation.HEIGHT/2, (int) (GlowAnimation.WIDTH/2 + 2000 * Math.cos(direction)), (int) (GlowAnimation.HEIGHT/2 + 2000 * Math.sin(direction)));
 //        g2d.setColor(new Color(red + (255 - red) / 8 * 4, green + (255 - green) / 8 * 4, blue + (255 - blue) / 8 * 4));
 //        g2d.setStroke(new BasicStroke((int) width / 4));
-//        g2d.drawLine(WIDTH/2, HEIGHT/2, (int) (WIDTH/2 + 2000 * Math.cos(direction)), (int) (HEIGHT/2 + 2000 * Math.sin(direction)));
+//        g2d.drawLine(GlowAnimation.WIDTH/2, GlowAnimation.HEIGHT/2, (int) (GlowAnimation.WIDTH/2 + 2000 * Math.cos(direction)), (int) (GlowAnimation.HEIGHT/2 + 2000 * Math.sin(direction)));
 //        g2d.setColor(new Color(red + (255 - red) / 8 * 8, green + (255 - green) / 8 * 8, blue + (255 - blue) / 8 * 8));
 //        g2d.setStroke(new BasicStroke((int) width / 8));
-//        g2d.drawLine(WIDTH/2, HEIGHT/2, (int) (WIDTH/2 + 2000 * Math.cos(direction)), (int) (HEIGHT/2 + 2000 * Math.sin(direction)));
+//        g2d.drawLine(GlowAnimation.WIDTH/2, GlowAnimation.HEIGHT/2, (int) (GlowAnimation.WIDTH/2 + 2000 * Math.cos(direction)), (int) (GlowAnimation.HEIGHT/2 + 2000 * Math.sin(direction)));
 //        g2d.setStroke(new BasicStroke(1));
     }
 
