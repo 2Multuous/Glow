42535af4e8797260347c81f2643896d0a699e9ab
2Multuous <132314186+2Multuous@users.noreply.github.com>
2024-05-19 11:09:38 -0400

    Added High Score Manager to the Code

---
+++ HighScoreManager.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class HighScoreManager {
+	
+	public static String createFile() {
+		File highScore = new File("highscore.txt");
+		try {
+			FileWriter f = new FileWriter("highscore.txt");
+			f.write(0);
+			return "highscore.txt";
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static int getScore() {
+		try {
+			FileReader f = new FileReader(createFile());
+			String s = "";
+			int o = f.read();
+			while(o != -1) {
+				s += o;
+			}
+			return Integer.parseInt(s);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public static void writeScore(int score) {
+		try {
+			FileWriter f = new FileWriter(createFile());
+			f.write(score);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
